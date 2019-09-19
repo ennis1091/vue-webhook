40a12ca7970a3b1810ff47348aa5a444217ab6cc
ennis <1091795872@qq.com>
2019-09-19 16:25:54 +0800

    webhook-gitee-github

--- webhook.js
+++ webhook.js
@@ -47,9 +47,15 @@ let server = http.createServer(function(req, res) {
               buffers.push(buffer);
             });
             child.stdout.on("end", () => {
-              let log = Buffer.concat(buffers);
-              console.log(log.toString());
-              console.log("自动化拉取完毕");
+              let logs = Buffer.concat(buffers).toString();
+              sendMail(`
+                  <h1>部署日期: ${new Date()}</h1>
+                  <h2>部署人: ${payload.pusher.name}</h2>
+                  <h2>部署邮箱: ${payload.pusher.email}</h2>
+                  <h2>提交信息: ${payload.head_commit &&
+                    payload.head_commit["message"]}</h2>
+                  <h2>布署日志: ${logs.replace("\r\n", "<br/>")}</h2>
+              `);
             });
           }
           // 返回的json, 配置在./src/resModel中
